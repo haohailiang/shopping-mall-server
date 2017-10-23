dbc4812d3a347668242ac029a5857aad85a7e079
haohailiang <646263409@qq.com>
2017-10-23 14:31:36 +0800

    用户的登录登出

--- routes/users.js
+++ routes/users.js
@@ -29,6 +29,10 @@ router.post('/login', (req, res, next) => {
 					path:'/',
 					maxAge:1000*60*60
 				});
+				res.cookie("userName",doc.userName,{
+					path:'/',
+					maxAge:1000*60*60
+				});
 				res.json({
 					status:"0",
 					msg:'',
@@ -39,6 +43,35 @@ router.post('/login', (req, res, next) => {
 			}
 		}
 	});
+});
+
+//登出接口
+router.post("/logout", function (req,res,next) {
+	res.cookie("userId","",{
+		path:"/",
+		maxAge:-1
+	});
+	res.json({
+		status:"0",
+		msg:'',
+		result:''
+	})
+});
+
+router.get("/checkLogin", (req,res,next) => {
+	if(req.cookies.userId){
+		res.json({
+			status:'0',
+			msg:'',
+			result:req.cookies.userName || ''
+		});
+	}else{
+		res.json({
+			status:'1',
+			msg:'未登录',
+			result:''
+		});
+	}
 });
 
 module.exports = router;
