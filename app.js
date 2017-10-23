dd324d7dc3516b6d2e4a562e16b0c8dcb107f312
haohailiang <646263409@qq.com>
2017-10-23 14:55:58 +0800

    比较path和originalUrl

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function (req,res,next) {
 	if(req.cookies.userId){
 		next();
 	}else{
-		console.log("url:"+req.originalUrl);
+		console.log(`path:${req.path}, originalUrl:${req.originalUrl}`);
 		if(req.originalUrl=='/users/login' || req.originalUrl=='/users/logout' || req.originalUrl.indexOf('/goods/list')>-1){
 			next();
 		}else{
