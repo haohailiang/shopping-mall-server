5266629adba0c67283791e2743e30c2d870bf2d9
haohailiang <646263409@qq.com>
2017-10-23 17:01:28 +0800

    我的购物车

--- routes/users.js
+++ routes/users.js
@@ -74,4 +74,26 @@ router.get("/checkLogin", (req,res,next) => {
 	}
 });
 
+//查询当前用户的购物车数据
+router.get("/cartList", (req,res,next) => {
+	var userId = req.cookies.userId;
+	User.findOne({userId:userId}, function (err,doc) {
+		if(err){
+			res.json({
+				status:'1',
+				msg:err.message,
+				result:''
+			});
+		}else{
+			if(doc){
+				res.json({
+					status:'0',
+					msg:'',
+					result:doc.cartList
+				});
+			}
+		}
+	});
+});
+
 module.exports = router;
