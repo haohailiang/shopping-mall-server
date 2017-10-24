8bf8468b0dd76612325b8bbcced05c578803e50f
haohailiang <646263409@qq.com>
2017-10-24 11:59:00 +0800

    删除购物车等大量操作

--- routes/users.js
+++ routes/users.js
@@ -96,4 +96,96 @@ router.get("/cartList", (req,res,next) => {
 	});
 });
 
+router.post("/editCheckAll", function (req,res,next) {
+	var userId = req.cookies.userId,
+		checkAll = req.body.checkAll?'1':'0';
+	User.findOne({userId:userId}, function (err,user) {
+		if(err){
+			res.json({
+				status:'1',
+				msg:err.message,
+				result:''
+			});
+		}else{
+			if(user){
+				user.cartList.forEach((item)=>{
+					item.checked = checkAll;
+				})
+				user.save(function (err1,doc) {
+					if(err1){
+						res.json({
+							status:'1',
+							msg:err1,message,
+							result:''
+						});
+					}else{
+						res.json({
+							status:'0',
+							msg:'',
+							result:'suc'
+						});
+					}
+				})
+			}
+		}
+	});
+});
+
+//修改商品数量
+router.post("/cartEdit", function (req,res,next) {
+	var userId = req.cookies.userId,
+		productId = req.body.productId,
+		productNum = req.body.productNum,
+		checked = req.body.checked;
+	User.update({
+		"userId":userId,
+		"cartList.productId":productId
+	},{
+		"cartList.$.productNum":productNum,
+		"cartList.$.checked":checked,
+	}, function (err,doc) {
+		if(err){
+			res.json({
+				status:'1',
+				msg:err.message,
+				result:''
+			});
+		}else{
+			res.json({
+				status:'0',
+				msg:'',
+				result:'suc'
+			});
+		}
+	})
+});
+
+//购物车删除
+router.post("/cartDel", function (req,res,next) {
+	var userId = req.cookies.userId,productId = req.body.productId;
+	User.update({
+		userId:userId
+	},{
+		$pull:{
+			'cartList':{
+				'productId':productId
+			}
+		}
+	}, function (err,doc) {
+		if(err){
+			res.json({
+				status:'1',
+				msg:err.message,
+				result:''
+			});
+		}else{
+			res.json({
+				status:'0',
+				msg:'',
+				result:'suc'
+			});
+		}
+	});
+});
+
 module.exports = router;
